2293edecbdec00ad95ed787473662d758987adfb
Patricia <patricia.badji89@gmail.com>
2025-03-24 12:17:24 +0100

    Update PlaylistGetter.jsx

--- src/components/PlaylistGetter.jsx
+++ src/components/PlaylistGetter.jsx
@@ -328,8 +328,8 @@ const PlaylistGetter = () => {
                           position: "absolute",
                           top: 0,
                           left: 0,
-                          width: "1%",
-                          height: "1%",
+                          width: "100%",
+                          height: "100%",
                           border: "none",
                         }}
                         src={`https://www.youtube.com/embed/${videoId}?autoplay=${
